d9c8dc67c2d89211989b6fd9b4d0ddd75614b247
nece001 <nece001@163.com>
2023-10-05 16:27:04 +0800

    修改分页

--- src/Paginator.php
+++ src/Paginator.php
@@ -20,7 +20,11 @@ class Paginator implements ArrayAccess, Countable, IteratorAggregate
     private $limit;
     private $total;
     private $current_page;
+    private $pre_page;
+    private $next_page;
     private $last_page;
+    private $link_start;
+    private $link_end;
     private $items = array();
 
     /**
@@ -32,13 +36,22 @@ class Paginator implements ArrayAccess, Countable, IteratorAggregate
      * @param integer $limit 每页数量
      * @param integer $total 总数量
      * @param integer $current_page 当前页码
+     * @param integer $link_count 分页链接数量
      */
-    public function __construct(int $limit, int $total, int $current_page)
+    public function __construct(int $limit, int $total, int $current_page, int $link_count = 10)
     {
         $this->limit = $limit;
         $this->total = $total;
         $this->current_page = $current_page;
         $this->last_page = ceil($this->total / $this->limit);
+        $this->pre_page = $this->current_page > 1 ? $this->current_page - 1 : 1;
+        $this->next_page = $this->current_page < $this->last_page ? $this->current_page + 1 : $this->last_page;
+
+        $middle = intval($link_count / 2);
+        $this->link_start = $this->current_page - $middle;
+        $this->link_start = $this->link_start >= 1 ? $this->link_start : 1;
+        $this->link_end = $this->link_start + $link_count;
+        $this->link_end = $this->link_end < $this->last_page ? $this->link_end : $this->last_page;
     }
 
     /**
@@ -211,6 +224,32 @@ class Paginator implements ArrayAccess, Countable, IteratorAggregate
     }
 
     /**
+     * 获取前一页页码
+     *
+     * @Author nece001@163.com
+     * @DateTime 2023-10-05
+     *
+     * @return integer
+     */
+    public function getPrePage(): int
+    {
+        return $this->pre_page;
+    }
+
+    /**
+     * 获取后一页页码
+     *
+     * @Author nece001@163.com
+     * @DateTime 2023-10-05
+     *
+     * @return integer
+     */
+    public function getNextPage(): int
+    {
+        return $this->next_page;
+    }
+
+    /**
      * 获取最后一页页码（总页数）
      *
      * @Author nece001@163.com
@@ -221,6 +260,32 @@ class Paginator implements ArrayAccess, Countable, IteratorAggregate
     public function getLastPage(): int
     {
         return $this->last_page;
+    }
+
+    /**
+     * 分页链接开始数
+     *
+     * @Author nece001@163.com
+     * @DateTime 2023-10-05
+     *
+     * @return integer
+     */
+    public function getLinkStart(): int
+    {
+        return $this->link_start;
+    }
+
+    /**
+     * 分页链接结束数
+     *
+     * @Author nece001@163.com
+     * @DateTime 2023-10-05
+     *
+     * @return integer
+     */
+    public function getLinkEnd(): int
+    {
+        return $this->link_end;
     }
 
     /**
@@ -251,6 +316,10 @@ class Paginator implements ArrayAccess, Countable, IteratorAggregate
             'total' => $this->getTotal(),
             'current_page' => $this->getCurrentPage(),
             'last_page' => $this->getLastPage(),
+            'pre_page' => $this->getPrePage(),
+            'next_page' => $this->getNextPage(),
+            'link_start' => $this->getLinkStart(),
+            'link_end' => $this->getLinkEnd(),
             'items' => $this->getItems()
         );
 
